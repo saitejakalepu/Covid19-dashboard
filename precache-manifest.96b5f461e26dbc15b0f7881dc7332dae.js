72d60f76037c9e04d382c45345fcb077818c5396
saitejababu <>
2020-09-26 22:23:24 +0530

    Updates

---
+++ precache-manifest.96b5f461e26dbc15b0f7881dc7332dae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be07c031039fce24517877bb51635a4f",
+    "url": "/Covid19-dashboard/index.html"
+  },
+  {
+    "revision": "f87c10846e5c7a8187b4",
+    "url": "/Covid19-dashboard/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "3476a30489ba7caa248c",
+    "url": "/Covid19-dashboard/static/css/main.3fa71f39.chunk.css"
+  },
+  {
+    "revision": "f87c10846e5c7a8187b4",
+    "url": "/Covid19-dashboard/static/js/2.871fa895.chunk.js"
+  },
+  {
+    "revision": "680d6675f03e59abc2c208f7cc877f04",
+    "url": "/Covid19-dashboard/static/js/2.871fa895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3476a30489ba7caa248c",
+    "url": "/Covid19-dashboard/static/js/main.16fa416e.chunk.js"
+  },
+  {
+    "revision": "e78cd72bf7dc1393df06",
+    "url": "/Covid19-dashboard/static/js/runtime-main.ba725b80.js"
+  }
+]);
